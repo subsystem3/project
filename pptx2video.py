9a8918893b5d8f806abe7798af8fcaa2be16455d
John Smith <5193877+jonathanagustin@users.noreply.github.com>
2023-07-10 03:12:15 -0400

    comment pptx2video class

--- pptx2video.py
+++ pptx2video.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import shutil
 import subprocess
+from typing import List
 
 from gtts import gTTS
 from moviepy.editor import AudioFileClip, ImageClip, concatenate_videoclips
@@ -24,13 +25,26 @@ class PPTXtoVideo:
             slide.notes_slide.notes_text_frame.text for slide in self.slides
         ]
 
-    def format_duration(self, duration):
-        """Formats a duration in seconds into a string in the format 'mm:ss'."""
+    def format_duration(self, duration: int) -> str:
+        """
+        Formats a duration in seconds into a string in the format 'mm:ss'.
+
+        Args:
+            duration (int): Duration in seconds.
+
+        Returns:
+            str: Formatted duration string.
+        """
         minutes, seconds = divmod(int(duration), 60)
         return f"{minutes}:{seconds:02}"
 
-    def write_metadata(self, videos):
-        """Writes metadata to a text file."""
+    def write_metadata(self, videos: List[AudioFileClip]):
+        """
+        Writes metadata to a text file.
+
+        Args:
+            videos (List[AudioFileClip]): List of video clips.
+        """
         total_duration = sum(video.duration for video in videos)
         with open(self.pptx_filename.replace(".pptx", ".txt"), "w") as f:
             f.write(f"Total duration: {self.format_duration(total_duration)}\n")
@@ -40,12 +54,19 @@ class PPTXtoVideo:
                 f.write(f"Voiceover: {self.voiceover_texts[i]}\n")
 
     def convert_to_pdf(self):
-        """Converts the .pptx file to a .pdf file using LibreOffice."""
+        """
+        Converts the .pptx file to a .pdf file using LibreOffice.
+        """
         cmd = f"libreoffice --headless --convert-to pdf {self.pptx_filename}"
         subprocess.run(cmd, shell=True, check=True)
 
-    def create_videos(self):
-        """Creates a video for each slide with a voiceover."""
+    def create_videos(self) -> List[AudioFileClip]:
+        """
+        Creates a video for each slide with a voiceover.
+
+        Returns:
+            List[AudioFileClip]: List of video clips.
+        """
         videos = []
         assets_dir = "assets"
         if os.path.exists(assets_dir):
@@ -65,13 +86,20 @@ class PPTXtoVideo:
             videos.append(video)
         return videos
 
-    def combine_videos(self, videos):
-        """Combines all the videos into one video."""
+    def combine_videos(self, videos: List[AudioFileClip]):
+        """
+        Combines all the videos into one video.
+
+        Args:
+            videos (List[AudioFileClip]): List of video clips.
+        """
         final_clip = concatenate_videoclips(videos, method="compose")
         final_clip.write_videofile(self.output_file, fps=24)
 
     def convert(self):
-        """Converts the PowerPoint presentation to a video."""
+        """
+        Converts the PowerPoint presentation to a video.
+        """
         self.convert_to_pdf()
         videos = self.create_videos()
         self.write_metadata(videos)
@@ -79,7 +107,9 @@ class PPTXtoVideo:
 
 
 def main():
-    """Main function to test the PPTXtoVideo class."""
+    """
+    Main function to test the PPTXtoVideo class.
+    """
     parser = argparse.ArgumentParser(
         description="Convert a PowerPoint presentation to a video."
     )
