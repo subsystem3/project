b7511adb8c225f716e38731f7fc8151fa91faf20
John Smith <5193877+jonathanagustin@users.noreply.github.com>
2023-07-13 02:31:54 -0400

    code-server automation

---
+++ dev/code-server/code-server-login.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# setup webdriver
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+
+# open the website
+driver.get("https://dev.subsystem3.ai/login")
+
+# find the elements for password input
+password_input = driver.find_element(
+    By.CSS_SELECTOR, "body > div > div > div.content > form > div > input.password"
+)
+
+# enter the password and submit
+password_input.send_keys("Hesitancy#Polygon0#Unless")
+password_input.send_keys(Keys.RETURN)
+
+# wait for the live share button to be clickable
+wait = WebDriverWait(driver, 10)
+live_share_button = wait.until(
+    EC.element_to_be_clickable(
+        (
+            By.CSS_SELECTOR,
+            "#ms-vsliveshare\.vsliveshare\.liveshare\.userStatusBarItem > a",
+        )
+    )
+)
+
+# click the button to start a live share session
+live_share_button.click()
+
+# click on the more info button
+more_info_button = driver.find_element(
+    By.CSS_SELECTOR,
+    "#list_id_15_0 > div.notification-list-item.expanded > div.notification-list-item-details-row > div.notification-list-item-buttons-container > a:nth-child(2)",
+)
+more_info_button.click()
---
+++ dev/code-server/install-chrome-automation.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo apt-get --yes update
+sudo apt-get install wget unzip
+
+# INSTALL CHROME
+wget --quiet --output-document - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get --yes update
+sudo apt-get --yes install google-chrome-stable
+
+# INSTALL CHROMEDRIVER
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+sudo apt-get install -y unzip
+unzip ~/chromedriver_linux64.zip -d ~/
+rm ~/chromedriver_linux64.zip
+sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chown root:root /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
