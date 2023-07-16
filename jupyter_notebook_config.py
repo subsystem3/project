30b0b9ba77ddb88a6b49fc34f001e745dd87d69b
John Smith <5193877+jonathanagustin@users.noreply.github.com>
2023-07-16 17:55:50 -0400

    update environment & configuration

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,16 +4,14 @@ from jupyter_core.paths import jupyter_config_dir
 from jupyter_server.services.contents.filemanager import FileContentsManager
 from traitlets.config import Config
 
-# define the configuration directory
+# DEFINE THE CONFIGURATION DIRECTORY
 config_dir = jupyter_config_dir()
 
-# define the configuration file path
+# DEFINE THE CONFIGURATION FILE PATH
 config_file = os.path.join(config_dir, "jupyter_notebook_config.py")
 
-# create a new Config instance
+# INITIALIZE AND CONFIGURE THE CONFIGURATION
 c = Config()
-
-# set the JupyterLabCodeFormatter options
 c.NotebookApp.contents_manager_class = FileContentsManager
 c.ContentsManager.default_jupytext_formats = "ipynb,py"
 c.NotebookApp.disable_check_xsrf = True
